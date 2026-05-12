1b967e06c055e6d10e1999a3f69fa199fe213921
Mirza Merdovic <mirzamerdovic@gmail.com>
2026-05-12 22:11:01 +0200

    Remove redundant templates index barrel

--- src/templates.ts
+++ src/templates.ts
@@ -1,1 +1,24 @@
-export * from './templates/index.js';
+export type {
+  ArtifactTemplateValidationResult,
+  BodyValidationIssue,
+  BodyValidationIssueKind,
+  BodyValidationIssueScope,
+  PreparedArtifactBody,
+  TemplateArtifactType,
+  TemplateDefinition,
+  TemplateFieldDefinition,
+  TemplateRef,
+  TemplateSectionDefinition,
+  TemplateSource,
+} from './templates/types.js';
+export { loadTemplate } from './templates/loader.js';
+export { throwFirstValidationIssue } from './templates/errors.js';
+export { formatTemplateRef } from './templates/formatters.js';
+export {
+  extractTemplateProvenance,
+  renderBodyWithTemplateProvenance,
+  renderTemplateProvenance,
+  stripTemplateProvenance,
+} from './templates/provenance.js';
+export { prepareArtifactBody, validateArtifactBody } from './templates/prepare.js';
+export { validateBodyAgainstTemplate, validateBodyAgainstTemplateDetailed } from './templates/body-validator.js';
